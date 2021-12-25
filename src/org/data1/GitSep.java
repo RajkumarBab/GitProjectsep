34c2dfbac3dd9d3f0e702c9d4879f529f3fac495
Rajkumarbab <rasuraj006@gmail.com>
2021-12-25 20:32:17 +0530

    Raj done his work

--- src/org/data1/GitSep.java
+++ src/org/data1/GitSep.java
@@ -1,6 +1,12 @@
 package org.data1;
 
 public class GitSep {
+	
+	
+	private void m1() {
+		System.out.println("Raj code added");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("data");
 		System.out.println("sample");

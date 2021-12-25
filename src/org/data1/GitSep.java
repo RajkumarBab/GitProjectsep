6a40e05074a7edaec720f34813b1622497e29933
Rajkumarbab <rasuraj006@gmail.com>
2021-12-25 20:49:25 +0530

    Raj added a data

--- src/org/data1/GitSep.java
+++ src/org/data1/GitSep.java
@@ -1,6 +1,11 @@
 package org.data1;
 
 public class GitSep {
+	
+	private void m2() {
+		System.out.println("Raj added a data");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("data");
 		System.out.println("sample");
